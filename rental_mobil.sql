f0a26aceee30354dcf353845b588251e7d8bb07c
Syaeful Anwar <67324634+anwarsyaeful@users.noreply.github.com>
2021-01-11 21:22:43 +0700

    Add files via upload

---
+++ rental_mobil.sql
@@ -0,0 +1,247 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 05, 2020 at 03:01 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `rental_mobil`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nama_admin` varchar(120) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `password` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id_customer` int(11) NOT NULL,
+  `nama` varchar(120) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `alamat` varchar(120) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `no_telepon` varchar(20) NOT NULL,
+  `no_ktp` varchar(50) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id_customer`, `nama`, `username`, `alamat`, `gender`, `no_telepon`, `no_ktp`, `password`, `role_id`) VALUES
+(3, 'Jeremy Thomas', 'thomas', 'Jl. Florida no. 45 Rt 15/004 Condet Jakarta Timur', 'laki-laki', '085700001398', '3328112210950002', 'ee11cbb19052e40b07aac0ca060c23ee', 2),
+(8, 'Syaeful Anwar', 'admin', 'Jl. Kenanga no. 22 Rt 15/04 Kalimati Tegal', 'Laki-Laki', '085700002296', '3328112210960002', '21232f297a57a5a743894a0e4a801fc3', 1),
+(9, 'joko santoso', 'joko', 'taman bahagia no 31 Brebes', 'Laki-Laki', '085700001398', '3328112210950002', 'ee11cbb19052e40b07aac0ca060c23ee', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mobil`
+--
+
+CREATE TABLE `mobil` (
+  `id_mobil` int(11) NOT NULL,
+  `kode_type` varchar(120) NOT NULL,
+  `merk` varchar(120) NOT NULL,
+  `no_plat` varchar(20) NOT NULL,
+  `warna` varchar(20) NOT NULL,
+  `tahun` varchar(4) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `denda` int(11) NOT NULL,
+  `ac` int(11) NOT NULL,
+  `supir` int(11) NOT NULL,
+  `mp3_player` int(11) NOT NULL,
+  `central_lock` int(11) NOT NULL,
+  `gambar` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mobil`
+--
+
+INSERT INTO `mobil` (`id_mobil`, `kode_type`, `merk`, `no_plat`, `warna`, `tahun`, `status`, `harga`, `denda`, `ac`, `supir`, `mp3_player`, `central_lock`, `gambar`) VALUES
+(8, 'SDN', 'Mitsubishi XPander', 'G 1456 DA', 'Red', '2020', '1', 400000, 50000, 1, 1, 1, 0, 'xpander-red-metallic.png'),
+(9, 'SDN', 'Mercedes Benz', 'G 2987 BE', 'Putih', '2019', '0', 500000, 50000, 0, 0, 0, 0, '5a21734b9ee9a6_5420974515121416436509.png'),
+(10, 'SDN', 'Honda CR-V', 'H 2987 BEC', 'Blue', '2019', '1', 0, 0, 0, 0, 0, 0, 'Deep_Ocean_Blue.png'),
+(11, 'SDN', 'Toyota Fortuner', 'H 4567 SAC', 'Putih', '2018', '1', 0, 0, 0, 0, 0, 0, 'car-white1.png'),
+(12, 'SDN', 'CRV', 'H 4567 SAC', 'Blue', '2019', '1', 500000, 100000, 0, 1, 1, 0, 'Deep_Ocean_Blue1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rental`
+--
+
+CREATE TABLE `rental` (
+  `id_rental` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `tanggal_rental` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `status_rental` varchar(50) NOT NULL,
+  `status_pengembalian` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_rental` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `id_mobil` int(11) NOT NULL,
+  `tanggal_rental` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `harga` varchar(120) NOT NULL,
+  `denda` varchar(120) NOT NULL,
+  `total_denda` varchar(120) NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `status_pengembalian` varchar(50) NOT NULL,
+  `status_rental` varchar(50) NOT NULL,
+  `bukti_pembayaran` varchar(120) NOT NULL,
+  `status_pembayaran` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_rental`, `id_customer`, `id_mobil`, `tanggal_rental`, `tanggal_kembali`, `harga`, `denda`, `total_denda`, `tanggal_pengembalian`, `status_pengembalian`, `status_rental`, `bukti_pembayaran`, `status_pembayaran`) VALUES
+(6, 9, 9, '2020-06-26', '2020-06-30', '500000', '50000', '100000', '2020-07-02', 'Kembali', 'Selesai', '3328112210960002_kartuDaftar.pdf', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type`
+--
+
+CREATE TABLE `type` (
+  `id_type` int(11) NOT NULL,
+  `kode_type` varchar(10) NOT NULL,
+  `nama_type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`id_type`, `kode_type`, `nama_type`) VALUES
+(1, 'SDN', 'Sedan'),
+(2, 'MNV', 'Minivan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id_customer`);
+
+--
+-- Indexes for table `mobil`
+--
+ALTER TABLE `mobil`
+  ADD PRIMARY KEY (`id_mobil`);
+
+--
+-- Indexes for table `rental`
+--
+ALTER TABLE `rental`
+  ADD PRIMARY KEY (`id_rental`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_rental`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`id_type`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id_customer` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `mobil`
+--
+ALTER TABLE `mobil`
+  MODIFY `id_mobil` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `rental`
+--
+ALTER TABLE `rental`
+  MODIFY `id_rental` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_rental` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `id_type` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
